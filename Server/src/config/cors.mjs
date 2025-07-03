92feb4e381824f8bdf568a65868ba725e077345f
pavel7991 <pasha.selin97@gmail.com>
2025-07-04 02:56:28 +0300

    deploy to railway

--- Server/src/config/cors.mjs
+++ Server/src/config/cors.mjs
@@ -1,5 +1,5 @@
 const corsOptions = {
-	origin: ['http://localhost:5173', 'http://localhost:5174', 'http://localhost:3000'], // Разрешаем запросы только с этого домена
+	origin: true, // Разрешаем запросы только с этого домена
 	methods: ['GET', 'POST', 'PUT', 'DELETE'], // Разрешаем только определенные методы
 	credentials: true, // Разрешаем передачу кук и заголовков авторизации
 	optionsSuccessStatus: 200 // Для старых браузеров}
