9c19f0a9a38f72cea5962354aa82ea7ed76b1525
pavel7991 <pasha.selin97@gmail.com>
2025-07-04 14:40:21 +0300

    upd cors origin

--- Server/src/config/cors.mjs
+++ Server/src/config/cors.mjs
@@ -1,5 +1,5 @@
 const corsOptions = {
-	origin: true, // Разрешаем запросы только с этого домена
+	origin: 'https://fullstack-rust-one.vercel.app/', // Разрешаем запросы только с этого домена
 	methods: ['GET', 'POST', 'PUT', 'DELETE'], // Разрешаем только определенные методы
 	credentials: true, // Разрешаем передачу кук и заголовков авторизации
 	optionsSuccessStatus: 200 // Для старых браузеров}
